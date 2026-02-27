d074b70a527f243b58fec8f0cd575f59aa5d9b3d
bru1git <bertirodriguez.bruno@gmail.com>
2026-02-26 23:21:13 -0300

    ultimo commit antes de la entrega

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 
 let datosUsuario = [];
 
-
 function crearCuenta(nombre, saldoInicial) {
     datosUsuario[0] = nombre;
     datosUsuario[1] = saldoInicial;
@@ -27,8 +26,21 @@ function retirar(saldo, monto) {
     }
 }
 
+
 let nombre = prompt("Ingresa tu nombre:");
-let saldoInicial = parseFloat(prompt("Ingresa tu saldo inicial:"));
+
+let saldoInicial;
+
+
+while (true) {
+    saldoInicial = parseFloat(prompt("Ingresa tu saldo inicial (solo números):"));
+
+    if (!isNaN(saldoInicial) && saldoInicial >= 0) {
+        break;
+    } else {
+        alert("Error: Debes ingresar un número válido mayor o igual a 0.");
+    }
+}
 
 crearCuenta(nombre, saldoInicial);
 
@@ -53,12 +65,22 @@ while (continuar) {
 
         case "2":
             let montoDeposito = parseFloat(prompt("¿Cuánto deseas depositar?"));
-            saldo = depositar(saldo, montoDeposito);
+
+            if (!isNaN(montoDeposito) && montoDeposito > 0) {
+                saldo = depositar(saldo, montoDeposito);
+            } else {
+                alert("Ingresa un número válido mayor que 0.");
+            }
             break;
 
         case "3":
             let montoRetiro = parseFloat(prompt("¿Cuánto deseas retirar?"));
-            saldo = retirar(saldo, montoRetiro);
+
+            if (!isNaN(montoRetiro) && montoRetiro > 0) {
+                saldo = retirar(saldo, montoRetiro);
+            } else {
+                alert("Ingresa un número válido mayor que 0.");
+            }
             break;
 
         case "4":
